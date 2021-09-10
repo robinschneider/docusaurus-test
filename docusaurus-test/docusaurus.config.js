46bdf3e855776ce52a6f5e8238a2504cdb3c8645
Robin Schneider <robin-schneider@outlook.de>
2021-09-10 18:08:34 +0200

    Changed URL

--- docusaurus-test/docusaurus.config.js
+++ docusaurus-test/docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://robinschneider.github.io',
-  baseUrl: '/docusaurus-test/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
